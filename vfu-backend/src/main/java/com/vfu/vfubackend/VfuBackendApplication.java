ecf745a4bc42a3aee955bf77aaaddc3d0fff7a6c
johnpranoy7 <47387210+johnpranoy7@users.noreply.github.com>
2026-01-28 17:00:21 -0500

    Init Project Structure

---
+++ vfu-backend/src/main/java/com/vfu/vfubackend/VfuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vfu.vfubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VfuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VfuBackendApplication.class, args);
+    }
+
+}
